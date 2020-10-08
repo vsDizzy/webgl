5599f92248b699d453dd49e504ce71a34b6d0ffe
Pavel <todizzy@gmail.com>
2020-10-08 08:34:46 +0300

    `

--- src/app.ts
+++ src/app.ts
@@ -1,52 +1,19 @@
 import { mat4 } from 'gl-matrix';
+import { gl } from './context.js';
+import { Builder } from './builder.js';
+import { load } from './load.js';
 
 var cubeRotation = 0.0;
 
-main();
+main().catch((e) => console.error(e));
 
 //
 // Start here
 //
-function main() {
-  const canvas = document.querySelector('#glcanvas') as HTMLCanvasElement;
-  const gl = canvas.getContext('webgl');
-
-  // If we don't have a GL context, give up now
-
-  if (!gl) {
-    alert(
-      'Unable to initialize WebGL. Your browser or machine may not support it.'
-    );
-    return;
-  }
-
-  // Vertex shader program
-
-  const vsSource = `
-    attribute vec4 aVertexPosition;
-    attribute vec2 aTextureCoord;
-    uniform mat4 uModelViewMatrix;
-    uniform mat4 uProjectionMatrix;
-    varying highp vec2 vTextureCoord;
-    void main(void) {
-      gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
-      vTextureCoord = aTextureCoord;
-    }
-  `;
-
-  // Fragment shader program
-
-  const fsSource = `
-    varying highp vec2 vTextureCoord;
-    uniform sampler2D uSampler;
-    void main(void) {
-      gl_FragColor = texture2D(uSampler, vTextureCoord);
-    }
-  `;
-
+async function main() {
   // Initialize a shader program; this is where all the lighting
   // for the vertices and so forth is established.
-  const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
+  const shaderProgram = await getProgram();
 
   // Collect all the info needed to use the shader program.
   // Look up which attributes our shader program is using
@@ -537,57 +504,11 @@ function drawScene(gl, programInfo, buffers, texture, deltaTime) {
   cubeRotation += deltaTime;
 }
 
-//
-// Initialize a shader program, so WebGL knows how to draw our data
-//
-function initShaderProgram(gl, vsSource, fsSource) {
-  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
-  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
-
-  // Create the shader program
-
-  const shaderProgram = gl.createProgram();
-  gl.attachShader(shaderProgram, vertexShader);
-  gl.attachShader(shaderProgram, fragmentShader);
-  gl.linkProgram(shaderProgram);
-
-  // If creating the shader program failed, alert
-
-  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-    alert(
-      'Unable to initialize the shader program: ' +
-        gl.getProgramInfoLog(shaderProgram)
-    );
-    return null;
-  }
-
-  return shaderProgram;
-}
-
-//
-// creates a shader of the given type, uploads the source and
-// compiles it.
-//
-function loadShader(gl, type, source) {
-  const shader = gl.createShader(type);
-
-  // Send the source to the shader object
-
-  gl.shaderSource(shader, source);
-
-  // Compile the shader program
-
-  gl.compileShader(shader);
-
-  // See if it compiled successfully
-
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    alert(
-      'An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader)
-    );
-    gl.deleteShader(shader);
-    return null;
-  }
-
-  return shader;
+async function getProgram() {
+  const builder = new Builder();
+  builder.compile(gl.VERTEX_SHADER, await load('data/vertex.glsl'));
+  builder.compile(gl.FRAGMENT_SHADER, await load('data/frag.glsl'));
+  const program = builder.link();
+  builder.dispose();
+  return program;
 }
