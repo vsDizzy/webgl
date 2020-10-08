5fcc1a771f4e37335820034394a51011216eef29
Pavel <todizzy@gmail.com>
2020-10-08 08:52:05 +0300

    jj

--- src/texture.ts
+++ src/texture.ts
@@ -1,22 +1,15 @@
 export function getTexture(gl: WebGL2RenderingContext, url: string) {
-  return new Promise((resolve, reject) => {
-    const img = new Image();
-    img.src = url;
-    const texture = gl.createTexture();
-    img.onload = () => {
-      gl.bindTexture(gl.TEXTURE_2D, texture);
-      const level = 0;
-      gl.texImage2D(
-        gl.TEXTURE_2D,
-        level,
-        gl.RGBA,
-        gl.RGBA,
-        gl.UNSIGNED_BYTE,
-        img
-      );
-      resolve(texture);
-    };
-  });
+  const image = new Image();
+  image.src = url;
+  const texture = gl.createTexture();
+  image.onload = () => {
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+
+    gl.generateMipmap(gl.TEXTURE_2D);
+  };
+
+  return texture;
 }
 
 export function getTextureBuffer(gl: WebGL2RenderingContext) {
